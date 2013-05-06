1fc4b2252a39e9dffc73f7fc31c4b282e3225f07
Santiago Lopez <santiago.lopez.pina@gmail.com>
2013-05-06 22:42:00 +0200

    Now the library it is compatible whit Arduino >1.0.3

--- ArduSnake/Worm.cpp
+++ ArduSnake/Worm.cpp
@@ -19,7 +19,12 @@
 
 #include <Servo.h>
 #include <Oscillator.h>
-#include "WProgram.h"
+#if defined(ARDUINO) && ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+  #include <pins_arduino.h>
+#endif
 #include "Worm.h"
 
 //-- Macro for converting from degrees to radians
